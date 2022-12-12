c7d5a4cbf9fe50043d96bffdbe1269cde382bc51
Dunga1113 <92216192+Dunga1113@users.noreply.github.com>
2022-12-12 21:06:25 +0700

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,10 +1,17 @@
 const { Pool } = require('pg')
 
-POSTGRES_USER = 'postgres'
+/*POSTGRES_USER = 'postgres'
 POSTGRES_HOST = 'containers-us-west-102.railway.app'
 POSTGRES_DB = 'railway'
 POSTGRES_PWD = '0NZZASGvCsKDqIvcFkQj'
 POSTGRES_PORT = '6843'
+*/
+
+POSTGRES_USER = 'postgres'
+POSTGRES_HOST = 'containers-us-west-153.railway.app'
+POSTGRES_DB = 'railway'
+POSTGRES_PWD = 'J4K3TWUDYKvmjf4BCXfK'
+POSTGRES_PORT = '8001'
 
 const pgConfig = {
   user: POSTGRES_USER,
